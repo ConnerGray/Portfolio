c5ba12e32051e4a60cbe31099c2aa912ed9ce5ba
Conner <connergray15@gmail.com>
2021-02-02 21:42:47 -0500

    Added hamburger for mobile nav

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+const menuicon = document.querySelector('hamburger');
+const navbar = document.querySelector('navbar');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('show');
+});
+
+menuicon.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+});
+
